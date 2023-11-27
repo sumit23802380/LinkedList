81de328298639da6f87923864f1e93e6fe4d7355
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-27 22:29:03 +0530

    Add : Created Simple LinkedList

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         intLL.head = new Node<>(56);
         intLL.head.next = new Node<>(30);
         intLL.head.next.next = new Node<>(70);
+        System.out.print("Head -> " + intLL.head.data);
+        System.out.println("-> " + intLL.head.next.data + " -> " + intLL.head.next.next.data + " -> null");
     }
 }
