faeab35f59606ba6647c2fe564a77e91d5a48ca3
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-27 22:35:23 +0530

    Add : AddToHead Method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,3 +1,13 @@
 public class LinkedList <T>{
     public Node<T> head;
+    public void addToHead(T data){
+        Node<T> newNode = new Node<>(data);
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> intLL = new LinkedList<>();
-        intLL.head = new Node<>(56);
-        intLL.head.next = new Node<>(30);
-        intLL.head.next.next = new Node<>(70);
+        intLL.addToHead(70);
+        intLL.addToHead(30);
+        intLL.addToHead(56);
         System.out.print("Head -> " + intLL.head.data);
         System.out.println("-> " + intLL.head.next.data + " -> " + intLL.head.next.next.data + " -> null");
     }
