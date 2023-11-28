be2fc9744be597865a9372b2b7cab1f616f7ea92
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-28 07:47:03 +0530

    Add : Description of functions

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,13 @@
+/*
+    *@desc : Data Structure LinkedList Generic Class
+ */
 public class LinkedList <T>{
     public Node<T> head;
+    /*
+     *@desc : add the new node to beginning of linked list
+     *@params : Generic data
+     *@return :
+     */
     public void addToHead(T data){
         Node<T> newNode = new Node<>(data);
         if(head == null){
@@ -10,6 +18,11 @@ public class LinkedList <T>{
             head = newNode;
         }
     }
+    /*
+     *@desc : add the new node at the end of linked list
+     *@params : Generic data
+     *@return :
+     */
     public void append(T data){
         Node<T> newNode = new Node<>(data);
         if(head == null){
@@ -22,18 +35,33 @@ public class LinkedList <T>{
         }
         temp.next = newNode;
     }
+    /*
+     *@desc : Insert the new node between two nodes a and b
+     *@params : Generic data , two nodes of generic type
+     *@return :
+     */
     public void insertBetweenTwoNodes(T data , Node<T> a , Node<T> b){
         Node<T> newNode = new Node<>(data);
         a.next = newNode;
         newNode.next = b;
         head = a;
     }
+    /*
+     *@desc : remove the first element from the beginning of linked list
+     *@params :
+     *@return :
+     */
     public void pop(){
         if(head==null){
             return;
         }
         head = head.next;
     }
+    /*
+     *@desc : removes the last element of the linked list
+     *@params :
+     *@return :
+     */
     public void popLast(){
         if(head == null || head.next == null){
             head = null;
@@ -45,6 +73,11 @@ public class LinkedList <T>{
         }
         temp.next = null;
     }
+    /*
+     *@desc : search the particular value in the linked list present or not
+     *@params : Generic value
+     *@return : boolean that it is present in linked list or not
+     */
     public boolean searchNode(T value){
         Node<T> temp = head;
         while(temp!=null){
@@ -55,6 +88,11 @@ public class LinkedList <T>{
         }
         return false;
     }
+    /*
+     *@desc : insert the new node after the search value node
+     *@params : search value , value to insert
+     *@return :
+     */
     public void insertAfterParticularNode(T searchValue , T value){
         Node<T> temp = head;
         while (temp!=null){
@@ -67,6 +105,14 @@ public class LinkedList <T>{
             temp = temp.next;
         }
     }
+    public void deleteParticularNode(T searchValue){
+        
+    }
+    /*
+     *@desc : prints the linked list
+     *@params :
+     *@return :
+     */
     public void display(){
         Node<T> temp = head;
         while (temp!=null){
